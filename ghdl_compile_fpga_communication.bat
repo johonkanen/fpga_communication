5e0453f0463ac9acf0d431cffc28d8eefb676d04
Jari Honkanen <jaarhonka@yahoo.com>
2023-06-04 14:26:17 +0300

    update .bat file to accept argument

--- ghdl_compile_fpga_communication.bat
+++ ghdl_compile_fpga_communication.bat
@@ -1,14 +1,10 @@
 echo off
+SET source=%1
 
-echo %project_root%
-FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-parse --show-toplevel`) DO (
-SET project_root=%%F
-)
-SET source=%project_root%/source
+ghdl -a --ieee=synopsys --std=08 %source%\hVHDL_fpga_interconnect/interconnect_configuration/data_15_address_15_bit_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%\hVHDL_fpga_interconnect/fpga_interconnect_pkg.vhd
 
-ghdl -a --ieee=synopsys --std=08 hVHDL_fpga_interconnect/interconnect_configuration/data_15_address_15_bit_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 hVHDL_fpga_interconnect/fpga_interconnect_pkg.vhd
-
-ghdl -a --ieee=synopsys --std=08 hVHDL_uart/uart_transreceiver/uart_rx/uart_rx_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 hVHDL_uart/uart_transreceiver/uart_tx/uart_tx_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 hVHDL_uart/uart_protocol/uart_protocol_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%\hVHDL_uart/uart_rx/uart_rx_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%\hVHDL_uart/uart_tx/uart_tx_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%\hVHDL_uart/uart_protocol/uart_protocol_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%\communications.vhd
