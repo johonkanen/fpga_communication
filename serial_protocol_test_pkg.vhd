3f9e85713c0783bb9cbc2765bdce728be783fa5b
Jari Honkanen <jaarhonka@yahoo.com>
2025-01-27 09:11:09 +0200

    add stream frame with configurable frame length to uart_protocol_test_pkg

--- serial_protocol_test_pkg.vhd
+++ serial_protocol_test_pkg.vhd
@@ -1,6 +1,7 @@
 
 LIBRARY ieee  ; 
     USE ieee.std_logic_1164.all  ; 
+    USE ieee.numeric_std.all  ; 
 
     use work.uart_protocol_pkg.all;
 
@@ -13,6 +14,11 @@ package uart_protocol_test_pkg is
 
     function read_frame ( address : natural)
         return base_array;
+
+    function stream_frame (
+        address : natural
+        ; stream_length : natural)
+    return base_array;
 
     function stream_frame ( address : natural)
         return base_array;
@@ -54,16 +60,31 @@ package body uart_protocol_test_pkg is
     function stream_frame
     (
         address : natural
+        ; stream_length : natural
     )
     return base_array
     is
         variable retval : base_array(0 to 5);
+        variable std_length : std_logic_vector(23 downto 0) := std_logic_vector(to_unsigned(stream_length, 24));
     begin
         retval(0) := std_logic_vector'(x"05");
         retval(1 to 2) := int_to_bytes(address);
-        retval(3 to 5) := (x"00", x"00", x"01");
+        retval(3 to 5) := (3 => std_length(23 downto 16)
+                        , 4 => std_length(15 downto 8)
+                        , 5 =>std_length(7 downto 0)
+                        );
 
         return retval;
+    end stream_frame;
+
+    function stream_frame
+    (
+        address : natural
+    )
+    return base_array
+    is
+    begin
+        return stream_frame(address, 1);
     end stream_frame;
 
 end package body;
